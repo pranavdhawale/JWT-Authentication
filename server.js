fc1d5074a6dec55305d8838645661ffe1fa7f7cf
Pranav Dhawale <pranavdhawale19@gmail.com>
2022-10-26 19:36:34 +0530

    configure 'server.js' & send req by 'GET http://localhost:3000/posts'

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-console.log("Hello World");
+const express = require('express');
+const app = express();
+
+const posts = [
+    {
+        username: 'Pranav',
+        title: 'Post 1'
+    },
+    {
+        username: 'Noddy',
+        title: 'Post 2'
+    }
+];
+
+app.get('/posts', (req, res) => {
+    res.json(posts);
+    }
+);
+
+
+app.listen(3000, () => {
+    console.log('Server started on port 3000');
+    }
+);
