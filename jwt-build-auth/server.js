509f90a6c0b6a284a2fd1d68282c851eefcf7172
Pranav Dhawale <pranavdhawale19@gmail.com>
2022-10-26 21:42:20 +0530

    create login route and successfully logs in

--- jwt-build-auth/server.js
+++ jwt-build-auth/server.js
@@ -29,6 +29,26 @@ app.post('/users', async (req, res) => {
 }
 );
 
+app.post('/users/login', async (req, res) => {
+    const user = users.find(user => user.name = req.body.name)
+    if (user == null) {
+        return res.status(400).send('Cannot find user')
+
+    }
+    try {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            res.send('Success')
+        }
+        else {
+            res.send('Not Allowed')
+        }
+    }
+    catch {
+        res.status(500).send()
+    }
+}
+);
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
