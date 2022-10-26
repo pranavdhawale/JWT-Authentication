e2428b25536472fc775a4242850472ef08d7d59b
Pranav Dhawale <pranavdhawale19@gmail.com>
2022-10-26 21:38:15 +0530

    set parameter while encrypting password with hash instead of using salt

--- jwt-build-auth/server.js
+++ jwt-build-auth/server.js
@@ -14,10 +14,10 @@ app.get('/users', (req, res) => {
 
 app.post('/users', async (req, res) => {
     try {
-        const salt = await bcrypt.genSalt()
-        const hashedPassword = await bcrypt.hash(req.body.password, salt)
-        console.log(salt)
-        console.log(hashedPassword)
+        // const salt = await bcrypt.genSalt()
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        // console.log(salt)
+        // console.log(hashedPassword)
 
         const user = {name : req.body.name, password : hashedPassword}
         users.push(user)
