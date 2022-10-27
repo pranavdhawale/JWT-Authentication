6817106344402bbc46d4d4e47643f5eb70601987
Pranav Dhawale <pranavdhawale19@gmail.com>
2022-10-27 08:38:39 +0530

    resolve issue while loggin 'DOES NOT LOG OTHER USERS'

--- jwt-auth/server.js
+++ jwt-auth/server.js
@@ -1,31 +1,42 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
+const express = require('express')
+const bcrypt = require('bcrypt')
 
-const app = express();
-app.use(express.json());
+const app = express()
+app.use(express.json())
 
-const posts = [
-    {
-        username: 'Pranav',
-        title: 'Post 1'
-    },
-    {
-        username: 'Noddy',
-        title: 'Post 2'
+const users = []
+
+app.get('/users', (req, res) => {
+  res.json(users)
+})
+
+app.post('/register', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    const user = { name: req.body.name, password: hashedPassword }
+    users.push(user)
+    res.status(201).send()
+  } catch {
+    res.status(500).send()
+  }
+})
+
+app.post('/login', async (req, res) => {
+  const user = users.find(user => user.name === req.body.name)
+  if (user == null) {
+    return res.status(400).send('Cannot find user')
+  }
+  try {
+    if(await bcrypt.compare(req.body.password, user.password)) {
+      res.send('Success')
+    } else {
+      res.send('Not Allowed')
     }
-];
-
-app.get('/posts', (req, res) => {
-    res.json(posts);
-    }
-);
-
-app.get('/login', (req, res) => {
-    res.send('Login');
-    }
-);
+  } catch {
+    res.status(500).send()
+  }
+})
 
 app.listen(3000, () => {
-    console.log('Server started on port 3000');
-    }
-);
+  console.log('Server is running on port 3000')
+});
--- jwt-build-auth/server.js
+++ jwt-build-auth/server.js
@@ -1,56 +1,42 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
+const express = require('express')
+const bcrypt = require('bcrypt')
 
-const app = express();
-
+const app = express()
 app.use(express.json())
 
 const users = []
 
 app.get('/users', (req, res) => {
-    res.json(users)
-    }
-);
+  res.json(users)
+})
 
-app.post('/users', async (req, res) => {
-    try {
-        // const salt = await bcrypt.genSalt()
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        // console.log(salt)
-        // console.log(hashedPassword)
+app.post('/register', async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    const user = { name: req.body.name, password: hashedPassword }
+    users.push(user)
+    res.status(201).send()
+  } catch {
+    res.status(500).send()
+  }
+})
 
-        const user = {name : req.body.name, password : hashedPassword}
-        users.push(user)
-        res.status(201).send()
+app.post('/login', async (req, res) => {
+  const user = users.find(user => user.name === req.body.name)
+  if (user == null) {
+    return res.status(400).send('Cannot find user')
+  }
+  try {
+    if(await bcrypt.compare(req.body.password, user.password)) {
+      res.send('Success')
+    } else {
+      res.send('Not Allowed')
     }
-    catch {
-        res.status(500).send()
-    }
-}
-);
+  } catch {
+    res.status(500).send()
+  }
+})
 
-app.post('/users/login', async (req, res) => {
-    const user = users.find(user => user.name = req.body.name)
-    if (user == null) {
-        return res.status(400).send('Cannot find user')
-
-    }
-    try {
-        if (await bcrypt.compare(req.body.password, user.password)) {
-            res.send('Success')
-        }
-        else {
-            res.send('Not Allowed')
-        }
-    }
-    catch {
-        res.status(500).send()
-    }
-}
-);
-
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-    }
-);
+app.listen(4000, () => {
+  console.log('Server is running on port 4000')
+});
