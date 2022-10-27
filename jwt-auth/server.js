37457b961235a8f39493714615a0d87a4a768f2f
Pranav Dhawale <pranavdhawale19@gmail.com>
2022-10-27 09:21:58 +0530

    access post by authenticating accessToken of user

--- jwt-auth/server.js
+++ jwt-auth/server.js
@@ -9,9 +9,29 @@ app.use(express.json())
 
 const users = []
 
+const posts = [
+    {
+        username: 'pranav',
+        title: 'Post 1'
+    },
+    {
+        username: 'deep',
+        title: 'Post 2'
+    },
+    {
+        username: 'noddy',
+        title: 'Post 3'
+    }
+]
+
 app.get('/users', (req, res) => {
     res.json(users)
 });
+
+app.get('/posts', authenticateToken, (req, res) => {
+    res.json(posts.filter(post => post.username === req.user.name))
+});
+
 
 app.post('/register', async (req, res) => {
     try {
@@ -23,6 +43,7 @@ app.post('/register', async (req, res) => {
         res.status(500).send()
     }
 });
+
 
 app.post('/login', async (req, res) => {
     const user = users.find(user => user.name === req.body.name)
@@ -44,6 +65,20 @@ app.post('/login', async (req, res) => {
         res.status(500).send()
     }
 });
+
+function authenticateToken(req, res, next) {
+    // Bearer TOKEN
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1] // if we have the authHeader then return the authHeader token portion which we spilt by the space
+    if (token == null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        console.log(err);
+        if (err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })
+}
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000')
